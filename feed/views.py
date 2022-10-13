2f25df743549d5407aaa9d5e72156717ce33fc2d
Abdelkader Amar <abdelkader.amar@gmail.com>
2022-10-13 21:31:12 +0200

    Adding pagination

--- feed/views.py
+++ feed/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 
 from .task import create_random_user_accounts
 from .models import TopStory
 
+def get_stories_page(request, stories):
+    paginator = Paginator(stories, 25)
+    page_number = request.GET.get('page')
+    return paginator.get_page(page_number)
+
+
 def index(request):
     print(f"Index")
     stories = TopStory.objects.all().order_by('-time')
-    context = {'stories': stories, }
+    stories_page = get_stories_page(request, stories)
+    context = {'stories': stories_page, }
     return render(request, 'feed/index.html', context)
 
 def feed_admin(request):
@@ -26,5 +34,6 @@ def sort(request, sort_param):
     elif sort_param == 'ratio':
         stories = list(TopStory.objects.all())
         stories.sort(key=lambda s : 0 if s.descendants == 0 else s.score/s.descendants, reverse=True)
-    context = {'stories': stories, }
+    stories_page = get_stories_page(request, stories)
+    context = {'stories': stories_page, }
     return render(request, 'feed/index.html', context)
